c64b415358e2fb14d2853f37176b49065da5937e
a88759666 <74145521+a88759666@users.noreply.github.com>
2023-03-23 23:14:53 +0800

    Update ProfileCard.tsx
    add noti

--- src/pages/user/components/ProfileCard.tsx
+++ src/pages/user/components/ProfileCard.tsx
@@ -1,3 +1,4 @@
+import { BellIcon, BellRingIcon, EmailIcon } from "assets/images"
 import { useNavigate } from "react-router-dom"
 
 
@@ -8,9 +9,12 @@ const ProfileCard = (props :{
   currentUserFollow:number,
   currentUserFollowed:number,
   coverUrl:string,
-  handleEdit:() => void
+  handleEdit:() => void,
+  type?: string
+  ringBell?: boolean
+  handleBellIcon: () => void
   }) => {
-    const {currentUserAccount, currentUserName, currentUserBio, currentUserFollow, currentUserFollowed, coverUrl, handleEdit} = props
+    const {currentUserAccount, currentUserName, currentUserBio, currentUserFollow, currentUserFollowed, coverUrl, handleEdit, type, ringBell, handleBellIcon} = props
     const go = useNavigate()
   return(
     <div className="flex flex-col h-1/2 relative">
@@ -37,12 +41,40 @@ const ProfileCard = (props :{
             >{currentUserFollowed}位</span>追隨者
           </p>
         </div>
-        <button  
-          className="rounded-[50px] px-4 py-2 border border-[#FF6600] bg-white text-[#FF6600] hover:opacity-50 absolute top-1/2 right-4 mt-4"
-          onClick={handleEdit}
-        >
-          編輯個人資料
-        </button>
+        {
+          type === 'user1' && (
+            <button  
+              className="rounded-[50px] px-4 py-2 border border-[#FF6600] bg-white text-[#FF6600] hover:opacity-50 absolute top-1/2 right-4 mt-4"
+              onClick={handleEdit}
+            >
+              編輯個人資料
+            </button>
+          )
+        }
+        {
+          type === 'user2' && (
+            <div className="flex flex-row absolute top-1/2 right-4 mt-4 gap-[16px] items-center">
+                <div className="w-[40px] h-[40px] p-[10px] rounded-full border-2 border-orange-400 cursor-pointer">
+                    <EmailIcon />
+                </div>
+                <div 
+                    className={`${
+                        ringBell ? 'bg-orange-400' : ''
+                    } w-[40px] h-[40px] p-[8px] rounded-full border-2 border-orange-400 cursor-pointer`}
+                    onClick={handleBellIcon}
+                >
+                    {
+                        ringBell ?   <BellRingIcon /> : <BellIcon />
+                    }
+                </div>
+                <button 
+                    className="btn-orange text-white focus:btn-orange-focus hover:btn-orange-hover"
+                >
+                    <h1 className="px-[15px] text-[15px] font-[700] leading-[15px]">正在跟隨</h1>
+                </button>
+            </div>
+          )
+        }
       </div>
       <img src="https://picsum.photos/300/300?text=2" alt="profile pic" className="absolute rounded-full width-[140px] h-[140px] top-[124px] left-4 border-4 border-white" />
     </div>
@@ -50,4 +82,3 @@ const ProfileCard = (props :{
 }
 
 export default ProfileCard;
-
