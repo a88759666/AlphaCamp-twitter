0f659b519cf1dde2ab003a99c340cb22938c2775
a88759666 <74145521+a88759666@users.noreply.github.com>
2023-03-23 20:40:26 +0800

    Update TweetContextProvider.tsx
    update

--- src/contexts/TweetContextProvider.tsx
+++ src/contexts/TweetContextProvider.tsx
@@ -1,5 +1,30 @@
 import { createContext, useContext } from "react"
-
+const friends = [
+  {
+    id: 1,
+    name: 'Apple',
+    account: '@appl3',
+    time: '3 小時',
+    avatar: 'https://picsum.photos/300/300?text=1',
+    showText: true,
+    text: 'Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum',
+    showFollow: false,
+    isFollowing: false,
+    showIcon: true,
+  },
+  {
+    id: 2,
+    name: 'dfrre',
+    account: '@rgtyl3',
+    time: '3 小時',
+    avatar: 'https://picsum.photos/300/300?text=2',
+    showText: true,
+    text: 'Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum',
+    showFollow: false,
+    isFollowing: true,
+    showIcon: true,
+  },
+]
 const dummydata = [
   {
   userName:"Apple", 
@@ -21,17 +46,26 @@ const dummydata = [
   }
 ]
 
-export const TweetContext = createContext(dummydata)
-export function useTweetContext(){ return useContext(TweetContext)}
+export const TweetContext = createContext({
+  dummydata,
+  friends
+})
+export function useTweetContext() { 
+  return useContext(TweetContext)
+}
 
 interface Props {children: React.ReactNode}
 
 const TweetContextProvider:React.FC<Props> = ({children}) => {
-  return(
-    <TweetContext.Provider value={dummydata}>
-      {children}
-    </TweetContext.Provider>
-  )
+  const providerValue = {
+    dummydata,
+    friends
+  }
+  return <>
+        <TweetContext.Provider value={providerValue}>
+            { children }
+        </TweetContext.Provider>
+    </>
 }
 
 export default TweetContextProvider
