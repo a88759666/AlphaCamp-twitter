e874e713fe547a16088f60cd70d653afd5260b8a
davelin18yufan <davelin30630@gmail.com>
2023-03-24 15:22:37 +0800

    [refactor]rename route name /admin/users

--- src/pages/admin/user/index.tsx
+++ src/pages/admin/user/index.tsx
@@ -1,5 +1,15 @@
+import AdminSideBar from "../components/AdminSideBar";
+import Container from "components/Container";
+
 const AdminUser: React.FC = () => {
-    return <p>AdminUser</p>;
+    return (
+      <Container>
+        <div className="basis-1/3 pl-[30px] py-4">
+          <AdminSideBar />
+        </div>
+      </Container>
+    )
+
   };
   
   export default AdminUser;
