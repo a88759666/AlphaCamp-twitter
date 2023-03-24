36d96d7d01fb50b72715e8316f79adba987419d3
davelin18yufan <davelin30630@gmail.com>
2023-03-24 17:34:54 +0800

    [feat]update admin/users page

--- src/pages/admin/user/index.tsx
+++ src/pages/admin/user/index.tsx
@@ -1,9 +1,20 @@
 import AdminSideBar from "../components/AdminSideBar";
 import Container from "components/Container";
 import { VectorIcon, LikeBigIcon } from "assets/images";
+import TweetContextProvider, { useTweetContext } from "contexts/TweetContextProvider";
 
-const UserCard = (props: {coverUrl:string, avatar:string, userName:string, userAccount:string }) => {
-  const {coverUrl, avatar, userName, userAccount} = props
+const UserCard = (props: {
+  coverUrl:string, 
+  avatar:string, 
+  userName:string, 
+  userAccount:string,
+  posts:number,
+  likes:number,
+  following:number,
+  followed:number
+  }) => {
+  const {coverUrl, avatar, userName, userAccount, posts, likes, following, followed} = props
+
   return(
     <div className="w-[210px] relative">
       <div className="h-[140px] overflow-hidden" 
@@ -20,51 +31,61 @@ const UserCard = (props: {coverUrl:string, avatar:string, userName:string, userA
         <div className="flex justify-center mt-[21px]">
           <div className="flex ">
             <VectorIcon/>
-            <p>1.5K</p>
+            <p className="ml-2">{posts}</p>
           </div>
           <div className="flex">
             <LikeBigIcon/>
-            <p className="">20K</p>
+            <p className="ml-1">{likes}</p>
           </div>
         </div>
         <div className="flex justify-center">
           <div className="flex mr-2">
             <p className="text-black text-[14px]">
-              34個
+              {following}個
               <span className="text-[#6C757D]">跟隨中</span>
             </p>
           </div>
           <div className="flex">
             <p className="text-black text-[14px]">
-              59位
-              <span className="text-[#6C757D]">追隨中</span>
+              {followed}位
+              <span className="text-[#6C757D]">跟隨者</span>
             </p>
           </div>
         </div>
-      </div>
-      <div>
-
       </div>
     </div>
   )
 }
 
 const AdminUser: React.FC = () => {
+  const{dummydata} = useTweetContext()
   return (
     <Container>
       <div className="flex flex-row h-screen">
         <div className="basis-1/4 pl-[30px] py-4">
           <AdminSideBar />
         </div>
-        <main className="basis-5/7 border-l ">
-          <header className="font-[700] text-[24px] leading-[26px] border-b border-slate-200 px-[20px] py-[24px]">使用者列表</header>
-          <UserCard 
-            coverUrl="https://picsum.photos/300/300?text=1"
-            avatar="https://picsum.photos/300/300?text=2"
-            userName="John Doe"
-            userAccount="heyJohn"
-            />
-        </main>
+        <TweetContextProvider >
+          <main className="basis-5/7 border-l ">
+            <header className="font-[700] text-[24px] leading-[26px] border-b border-slate-200 px-[20px] py-[24px]">使用者列表</header>
+            <div className="grid grid-flow-row grid-cols-4 gap-4 p-4">
+              {dummydata.map(item => {
+                return(
+                  <UserCard 
+                    coverUrl="https://picsum.photos/300/300?text=1"
+                    avatar={item.avatar}
+                    userName={item.userName}
+                    userAccount={item.account}
+                    posts={1.5}
+                    likes={20}
+                    following={36}
+                    followed={59}
+                    />
+                )
+              })}
+            </div>
+          </main>
+        </TweetContextProvider>
       </div>
     </Container>
   )
