163c45bcbf14faa97e8fce477fbff036e53b719c
a88759666 <74145521+a88759666@users.noreply.github.com>
2023-03-25 02:08:59 +0800

    Update index.tsx

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,47 @@
 import { InputCard, SubmitBtn, LogoTitle, Container} from "../../components/AuthInput"
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 
 const Login: React.FC = () => {
+  const [ account, setAccount ] = useState('')
+  const [ password, setPassword ] = useState('')
   const navigate = useNavigate()
+  function onChangeAccountHandler(event: React.FormEvent<HTMLInputElement>) {
+    if (event.currentTarget) {
+        setAccount(event.currentTarget.value)
+    }
+  }
+  function onChangePasswordHandler(event: React.FormEvent<HTMLInputElement>) {
+    if (event.currentTarget) {
+      setPassword(event.currentTarget.value)
+    }
+  }
     return (
       <Container>
         <LogoTitle title="登入 Alphitter"/>
-        <InputCard label="帳號" placeholder="請輸入帳號"/>
-        <InputCard label="密碼" placeholder="請輸入密碼"/>
+        <InputCard 
+          type='text'
+          name='account'
+          id="account"
+          label="帳號"
+          value={account}
+          onChange={onChangeAccountHandler}
+          placeholder="請輸入帳號"
+          wSize="small"
+          hSize="small"
+        />
+        <InputCard
+          type='password'
+          name='password'
+          id="password"
+          label="密碼"
+          value={password}
+          onChange={onChangePasswordHandler}
+          placeholder="請輸入密碼"
+          wSize="small"
+          hSize="small"
+        />
         <SubmitBtn btn="登入"/>
         <div className="mt-6 flex">
             <p className="flex-1 text-end link" onClick={() => navigate("/register")}>註冊</p>
