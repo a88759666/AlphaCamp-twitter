dddd29001e80214d1a9a17c00958d0472a5efb6f
davelin18yufan <davelin30630@gmail.com>
2023-03-24 14:26:22 +0800

    [refactor]restyle LogoTitle from AuthInput

--- src/pages/admin/login/index.tsx
+++ src/pages/admin/login/index.tsx
@@ -1,5 +1,19 @@
-const AdminLogin: React.FC = () => {
-    return <p>AdminLogin</p>;
+import { InputCard, SubmitBtn, LogoTitle, Container } from "components/AuthInput";
+
+const AdminLogin = () => {
+    return (
+      <Container>
+        <LogoTitle title="後台登入"/>
+        <InputCard label="帳號" placeholder="請輸入帳號"/>
+        <InputCard label="密碼" placeholder="請輸入密碼"/>
+        <SubmitBtn btn="登入"/>
+        <div className="mt-6 flex">
+            <p className="flex-1 text-end link">註冊</p>
+            <span className="block px-[20px]">&#8729;</span>
+            <p className="link">後台登入</p>
+        </div>
+      </Container>
+    )
   };
   
   export default AdminLogin;
