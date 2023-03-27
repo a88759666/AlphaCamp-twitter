1a2520bb4ab06591cb7e1e088760b71e0a96b2d8
a88759666 <74145521+a88759666@users.noreply.github.com>
2023-03-28 01:03:41 +0800

    Update index.tsx
    login authApi

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,14 @@
 import { InputCard, SubmitBtn, LogoTitle, Container} from "../../components/AuthInput"
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { checkPermissionUser, login } from "api/Auth";
 
 
 const Login: React.FC = () => {
   const [ account, setAccount ] = useState('')
   const [ password, setPassword ] = useState('')
-  const navigate = useNavigate()
+  const [ userId, setUserId ] = useState()
+  const go = useNavigate()
   function onChangeAccountHandler(event: React.FormEvent<HTMLInputElement>) {
     if (event.currentTarget) {
         setAccount(event.currentTarget.value)
@@ -17,6 +19,44 @@ const Login: React.FC = () => {
       setPassword(event.currentTarget.value)
     }
   }
+  async function handleClickLogin() {
+    if (account.length === 0) {
+      return;
+    }
+    if (password.length === 0) {
+      return;
+    }
+   
+    const { success, token, id } = await login({
+      account,
+      password,
+    });
+    
+    if (success) {
+      localStorage.setItem('token', token);
+      localStorage.setItem('userId', id)
+      // go('/home')
+      setUserId(id)
+    }
+    
+  }
+  useEffect(() => {
+    const checkTokenIsValid = async () => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        return;
+      }
+      
+      if(userId) {
+        const result = await checkPermissionUser(token, userId);
+        if (result) {
+          go('/home');
+        }
+      }
+    }
+    checkTokenIsValid();
+  }, [go, userId]);
+  
     return (
       <Container>
         <LogoTitle title="登入 Alphitter"/>
@@ -42,11 +82,14 @@ const Login: React.FC = () => {
           wSize="small"
           hSize="small"
         />
-        <SubmitBtn btn="登入"/>
+        <SubmitBtn 
+          btn="登入"
+          onClickEvent={handleClickLogin}
+        />
         <div className="mt-6 flex">
-            <p className="flex-1 text-end link" onClick={() => navigate("/register")}>註冊</p>
+            <p className="flex-1 text-end link" onClick={() => go("/register")}>註冊</p>
             <span className="block px-[20px]">&#8729;</span>
-            <p className="link" onClick={() => navigate("/admin/login")}>後台登入</p>
+            <p className="link" onClick={() => go("/admin/login")}>後台登入</p>
         </div>
       </Container>
     )
