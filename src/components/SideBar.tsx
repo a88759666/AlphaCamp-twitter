822b549a19b70456929dd2c71fb460a57ced001c
a88759666 <74145521+a88759666@users.noreply.github.com>
2023-03-28 01:06:07 +0800

    Update SideBar.tsx
    sideBar authApi

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,27 +1,47 @@
 import { ACLogoSmallIcon, HomeIcon, ProfileIcon, SettingIcon, LogoutIcon } from 'assets/images';
+import { useNavigate } from 'react-router-dom';
 
 
 
 const SideBar: React.FC = () => {
+    const go = useNavigate()
+    function handleClickSignout() {
+        localStorage.removeItem('token')
+        go('/login')
+    }
     return (
         <div className='h-full flex flex-col ml-[69px] items-start mr-[24px]'>
             <ACLogoSmallIcon />
-            <div className='flex flex-row gap-[16px] mt-[30px]'>
+            <div 
+                className='flex flex-row gap-[16px] mt-[30px] cursor-pointer'
+                onClick={() => go('/home')}
+            >
                 <HomeIcon />
                 <h1 className="font-[700] text-[18px] leading-[26px]" >首頁</h1>
             </div>
-            <div className='flex flex-row gap-[16px] mt-[40px]'>
+            <div 
+                className='flex flex-row gap-[16px] mt-[40px] cursor-pointer'
+                onClick={() => go('/user')}
+            >
                 <ProfileIcon />
                 <h1 className="font-[700] text-[18px] leading-[26px]">個人資料</h1>
             </div>
-            <div className='flex flex-row gap-[16px] mt-[40px]'>
+            <div 
+                className='flex flex-row gap-[16px] mt-[40px] cursor-pointer'
+                onClick={() => go('/setting')}
+            >
                 <SettingIcon />
                 <h1 className="font-[700] text-[18px] leading-[26px]">設定</h1>
             </div>
-            <button className="w-full btn-orange mt-[24px] py-[8px]">
+            <button 
+                className="w-full btn-orange mt-[24px] py-[8px]"
+            >
                 <h1 className="font-[500] text-white text-[18px] leading-[18px]">推文</h1>
             </button>
-            <div className='flex flex-row gap-[16px] mt-auto'>
+            <div 
+                className='flex flex-row gap-[16px] mt-auto cursor-pointer'
+                onClick={handleClickSignout}
+            >
                 <LogoutIcon />
                 <h1 className="font-[700] text-[18px] leading-[26px]">登出</h1>
             </div>
