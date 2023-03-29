1d8c1a4445a91fcdc355f6b814bc44a7cf37190a
a88759666 <74145521+a88759666@users.noreply.github.com>
2023-03-29 15:34:56 +0800

    Update index.tsx

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -3,13 +3,19 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { register } from "api/Auth";
 
-
-const Register = () => {
+const Register:React.FC = () => {
   const [ account, setAccount ] = useState('')
   const [ name, setName ] = useState('')
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ checkPassword, setCheckPassword ] = useState('')
+  const [ accountError, setAccountError ] = useState(false)
+  const [ nameError, setNameError ] = useState(false)
+  const [ colError, setColError ] = useState(false)
+  const [ emailError, setEmailError ] = useState(false)
+  const [ passwordError, setPasswordError ] = useState(false)
+
+  const [ errorMsg, setErrorMsg ] = useState('')
   const go = useNavigate()
 
   function onChangeAccountHandler(event: React.FormEvent<HTMLInputElement>) {
@@ -64,7 +70,23 @@ const Register = () => {
     if (success) {
       console.log(success)
       go('/login')
-      
+    } else {
+      let errorMsg = localStorage.getItem('errorMsg')
+      if( errorMsg === 'Error: account 已重複註冊!'){
+        setAccountError(true)
+      } 
+      if( errorMsg = 'Error: 暱稱上限50字!'){
+        setNameError(true)
+      } 
+      if( errorMsg = 'Error: 還有欄位沒填'){
+        setColError(true)
+      } 
+      if( errorMsg = 'Error: email 已重複註冊!'){
+        setEmailError(true)
+      } 
+      if( errorMsg = 'Error: 密碼與確認密碼不同!'){
+        setPasswordError(true)
+      }
     }
     
   }
@@ -81,6 +103,8 @@ const Register = () => {
           onChange={onChangeAccountHandler}
           wSize="small"
           hSize="small"
+          showError={accountError}
+          ErrorText="帳戶已經註冊過"
         />
         <InputCard 
           label="名稱" 
@@ -92,6 +116,8 @@ const Register = () => {
           onChange={onChangeNameHandler}
           wSize="small"
           hSize="small"
+          showError={nameError}
+          ErrorText="暱稱上限50字"
         />
         <InputCard 
           label="Email" 
@@ -102,7 +128,9 @@ const Register = () => {
           value={email}
           onChange={onChangeEmailHandler}
           wSize="small"
-          hSize="small" 
+          hSize="small"
+          showError={emailError}
+          ErrorText="信箱已經註冊過" 
         />
         <InputCard 
           label="密碼" 
@@ -113,7 +141,9 @@ const Register = () => {
           value={password}
           onChange={onChangePasswordHandler}
           wSize="small"
-          hSize="small" 
+          hSize="small"
+          showError={passwordError}
+          ErrorText="密碼與確認密碼不同"  
         />
         <InputCard 
           label="密碼確認" 
@@ -124,15 +154,24 @@ const Register = () => {
           value={checkPassword}
           onChange={onChangeCheckPasswordHandler}
           wSize="small"
-          hSize="small" 
+          hSize="small"
+          showError={colError}
+          ErrorText="有欄位是空的"  
         />
         <SubmitBtn 
           btn="註冊"
           onClickEvent={handleClickRegister} 
         />
         <div className="mt-6">
-          <p className="link text-center" onClick={() => go("/login")}>取消</p>
-        </div>
+            <p className="link text-center" onClick={() => go("/login")}>取消</p>
+        </div>   
+      </Container>
+    )
+  };
+  
+  export default Register;
+  
+
       </Container>
     )
   };
