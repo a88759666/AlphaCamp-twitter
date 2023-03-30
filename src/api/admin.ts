3c24fbe97230c9ee4e2fa3af8d86ec9216fbd3ce
a88759666 <74145521+a88759666@users.noreply.github.com>
2023-03-30 15:07:48 +0800

    Update admin.ts

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
+import { AdminTweetList } from 'type';
 
-const baseUrl = 'https://arcane-beyond-08221.herokuapp.com/api/admin';
+const baseUrl = 'https://arcane-beyond-08221.herokuapp.com/api/';
 
+interface payloadType {
+  account?: string,
+  password?: string,
+}
 interface User  {
   id: number,
   account: string,
@@ -34,7 +39,60 @@ axiosInstance.interceptors.request.use((config) => {
     console.log("axios interceptors error", error)
 })
 
+export async function adminLogin(payload:payloadType) {
+    
+  try {
+      const { account, password } = payload
+      const { data } = await axiosInstance.post(`${baseUrl}/signin`, {
+          account,
+          password
+      })
+      // console.log(data)
+      const { token, user } = data.data
+      const { id } = user
+      
+      if(token) {
+          return {
+              success: true,
+              ...data.data,
+              id
+          }
+      } else {
+          return data.data
+      }
+  } catch (error) {
+      console.error('login admin failed: 帳號未註冊', error)
+      return error
+  }
+}
+
+
+export async function adminLogout() {
+  localStorage.removeItem('token')
+  localStorage.removeItem('userId')
+}
+
+export async function adminGetTweets() {
+  try {
+      const res =  await axiosInstance.get(`${baseUrl}/admin/tweets`)
+      return res.data as AdminTweetList[]
+      
+  } catch (error) {
+      console.error('Get tweets failed:', error)
+  }
+}
+
+export async function adminDeleteTweets(id: number) {
+  try {
+      const res =  await axiosInstance.delete(`${baseUrl}/admin/tweets/${id}`)
+      return res.data as AdminTweetList[]
+  } catch (error) {
+      console.error('delete tweets failed:', error)
+  }
+}
+
+
 export const getUsers = async () => {
-  const res = await axiosInstance.get(`${baseUrl}/users`)
+  const res = await axiosInstance.get(`${baseUrl}/admin/users`)
   return res.data as User[]
 }
